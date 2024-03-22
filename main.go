dbb2d53e25be0c1d2f3fa51dfb6037453a224a61
ahmetbayirli <ahmet.bayirli@grapecity.com>
2024-03-22 15:11:49 +0300

    render template

--- main.go
+++ main.go
@@ -3,12 +3,31 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"text/template"
 )
 
+func HomeHandler(responseWriter http.ResponseWriter, requestPointer *http.Request) {
+	renderTemplate(responseWriter, "home.page.tmpl")
+}
+
+func renderTemplate(responseWriter http.ResponseWriter, templateName string) {
+	parsedTemplate, err := template.ParseFiles("./templates/" + templateName)
+
+	handleError(err)
+
+	err = parsedTemplate.Execute(responseWriter, nil) // second argument no Data
+
+	handleError(err)
+}
+
+func handleError(err error) {
+	if err != nil {
+		fmt.Println(err)
+	}
+}
+
 func main() {
-	http.HandleFunc("/",  func(responseWriter http.ResponseWriter, requestPointer *http.Request){
-		fmt.Fprintf(responseWriter , "Hello World")
-	})
+	http.HandleFunc("/", HomeHandler)
 
 	http.ListenAndServe(":8080", nil)
 }
